062d15f2e99ff25270a74bfad75d8c3e56077357
YilloGMR <iwangpetradheerendra@gmail.com>
2026-03-14 18:33:30 +0700

    Fix missing stdarg.h include for va_list support

--- include/holylib.h
+++ include/holylib.h
@@ -3,11 +3,13 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdarg.h>
 
-// v1.1 (2026-03-14)
+// v1.1.1 (2026-03-14)
 
 void clean();
 void pause();
 int holyscanf(void *output, char type, int size);
+int holysscanf(const char *type, int size, ...);
 
 #endif
